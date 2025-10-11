f065264214b4eccb930d2cac1a5719ab9acd5ee9
Jack Rubacha <rubacha.jack03@gmail.com>
2025-10-11 19:35:35 -0400

    formatter

---
+++ format.sh
@@ -0,0 +1,26 @@
+# requires jq and sponge from moreutils
+
+
+if [ "$1" = "check" ];
+then
+    failed=0
+    echo "Checking files"
+    for file in `find ./can-messages -type f`;
+    do
+    if ! diff -q <(jq . "$file") "$file" > /dev/null;
+    then
+	echo "FILE FAILED FORMAT CHECK: $file"
+        failed=1
+    else
+        echo "$file"
+    fi
+    done
+    exit "$failed" 
+else
+    echo "Formatting files"
+    for file in `find ./can-messages -type f`;
+    do
+       echo "$file"
+       jq . "$file" | sponge "$file"		
+    done
+fi
