5fc8a314c014e2f8a51f6c3f8b0afffe696cfb0e
Blake Jackson <blakejackson312006@gmail.com>
2026-01-18 17:39:44 -0500

    Added a VCU.json file and Lightning.json (#16)
    * Added first two messages for vcu.json
    
    * Made it more good
    
    * no more mpu.json
    
    * added trailing newline
    
    * added faults message
    
    * Added Dashboard eFuse message
    
    * removed unused field from faults message
    
    * no more illegal characters
    
    * added all of the eFuse messages
    
    * Removed illegal message descriptions for pump1 and pump2
    
    * Changed name of reverse bit in VCU State message
    
    * Made changes for code generation
    
    * Added new messages for the peripherals thread
    
    * Made SHT30 humidity not signed
    
    * Changed SHT30 humidity unit to %
    
    * Removed illegal characters from Temp sensor message description
    
    * customize to match vcu layout
    
    * fix short lightning message
    
    * fixup some rule fails
    
    * Made stuff big endian instead of little endian
    
    * Changed temp sensor CANID to 0x508 to match cerb
    
    * untested try struct encode
    
    * working bitstream encode
    
    * bitstream signed support
    
    * working struct generation
    
    * docs
    
    * more docs
    
    * updated 0x123 shutdown message
    
    * removed another little endian
    
    * lightning stuff
    
    * updated faults message layout
    
    * added extra bits to faults message so it's byte aligned
    
    * added options sim to last fault
    
    * added datatype to EXTRA so codegen works
    
    * changed pedal message points to use float instead of uint16_t
    
    * attempt to handle extended ids correctly
    
    * fix ext id
    
    * updated efuse messages to extended CAN ID
    
    * added thing to jninja
    
    * Added LV Voltage (#17)
    
    * Added LV voltage message
    
    * nvm only 32 bits
    
    * why not
    
    ---------
    
    Co-authored-by: Jack Rubacha <rubacha.jack03@gmail.com>
    Co-authored-by: Daniel Nakhooda <dnakhooda@gmail.com>

--- code-gen/codegen.py
+++ code-gen/codegen.py
@@ -43,12 +43,19 @@ def main(
     json_filtered_2 = []
     for msg in json_filtered:
         good = False
+        msg["use_struct"] = False
         for pt in msg["points"]:
             if "name" in pt and "c_type" in pt:
                 good = True
-                break
+
+            # if we detect little endian points, tell jinja to use the struct mode
+            if "endianness" in pt and pt["endianness"] == "little":
+                msg["use_struct"] = True
+                print(f"Warning: using struct generation for message {msg["desc"]}")
         if good:
             json_filtered_2.append(msg)
+        else:
+            print(f"Warning: cannot generate message {msg["desc"]}")
 
     print(
         f"Found {len(json_filtered_2)} eligible messages to generate code for of {len(json_filtered)} existing messages!"
